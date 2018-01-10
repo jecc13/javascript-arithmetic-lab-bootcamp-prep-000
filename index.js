6c84490a98bb9359601593a3a914c6ffbcdbd718
Jennifer <je@jenniferevenson.com>
2018-01-10 06:18:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,19 +25,12 @@ function dec(n) {
 function makeInt(n) {
   return parseInt(n, 10);
 }
+
+function preserveDecimal(n) {
+  return parseFloat(n);
+}
  
  /* 
-  makeInt(a.toString())).toEqual(n)
-  })
-
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})
 
 describe('preserveDecimal(n)', function() {
   it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
